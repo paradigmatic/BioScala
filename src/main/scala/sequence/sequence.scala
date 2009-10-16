a9c662cd37529c9e9e8aac117caa71f092e7faf1
JL Falcone <jean-luc.falcone@unige.ch>
2009-10-16 18:19:27 +0200

    Added value equality based on sequence only

--- src/main/scala/sequence/sequence.scala
+++ src/main/scala/sequence/sequence.scala
@@ -1,9 +1,13 @@
 package bio
 
-class Sequence[S]( val name: String, private val symSeq : Array[S] ) {
+import bio.symbols.Symbol
+
+class Sequence[S <: Symbol]( val name: String, private val symSeq : List[S] ) {
   
   val length = symSeq.length
 
   def apply(i: Int) = symSeq(i)
+
+  def ==( that: Sequence[S] ) = that.symSeq == this.symSeq
   
 }
--- src/test/scala/sequence.scala
+++ src/test/scala/sequence.scala
@@ -4,16 +4,26 @@ import bio.symbols.dna._
 
 class SequenceTest extends org.scalatest.FunSuite {
   test( "Sequence  length" ) {
-    val dna = Array( G, A, T, A, T , C )
+    val dna = List( G, A, T, A, T , C )
     val name = "Test sequence"
     val seq = new Sequence( name, dna )
     assert( 6 === seq.length )
   }
 
   test( "symbol accessor" ) {
-    val dna = Array( G, A, T, A, T , C )
+    val dna = List( G, A, T, A, T , C )
     val name = "Test sequence"
     val seq = new Sequence( name, dna )
     assert( dna(2) === seq(2) )
+  }
+
+  test( "value equality" ) {
+    val dna = List( G, A, T, A, T , C )
+    val name = "Test sequence"
+    val seq = new Sequence( name, dna )
+    val dna2 = List( G, A, T, A, T , C )
+    val name2 = "Test sequence"
+    val seq2 = new Sequence( name, dna2 )
+    assert( seq == seq2 )
   }
 }
