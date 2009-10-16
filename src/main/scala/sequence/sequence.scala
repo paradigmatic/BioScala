8f5b0d6c33c35ec8a95aa9677c143f6401fc90e6
JL Falcone <jean-luc.falcone@unige.ch>
2009-10-16 18:37:02 +0200

    Added string representation and mutation method to sequence

--- src/main/scala/sequence/sequence.scala
+++ src/main/scala/sequence/sequence.scala
@@ -9,5 +9,18 @@ class Sequence[S <: Symbol]( val name: String, private val symSeq : List[S] ) {
   def apply(i: Int) = symSeq(i)
 
   def ==( that: Sequence[S] ) = that.symSeq == this.symSeq
+
+  override def toString() = symSeq.map( _.toString ).mkString("")
+
+  def mutate( i: Int, newSym: S ): Sequence[S] = {
+    val newSymSeq = symSeq.take(2) ::: ( newSym :: symSeq.drop(3) )
+    new Sequence( name, newSymSeq )
+  }
+
   
 }
+
+import bio.symbols.dna.Nucleotide
+
+class DNA( name: String, dnaSeq: List[Nucleotide] )
+extends Sequence[Nucleotide](name,dnaSeq)
--- src/test/scala/sequence.scala
+++ src/test/scala/sequence.scala
@@ -2,28 +2,39 @@ import bio.Sequence
 import bio.symbols._
 import bio.symbols.dna._
 
-class SequenceTest extends org.scalatest.FunSuite {
+import org.scalatest._
+
+class SequenceTest extends FunSuite with BeforeAndAfter {
+
+  val dna = List( G, A, T, A, T , C )
+  val name = "Test sequence"
+  val seq = new Sequence( name, dna )
+  
   test( "Sequence  length" ) {
-    val dna = List( G, A, T, A, T , C )
-    val name = "Test sequence"
-    val seq = new Sequence( name, dna )
     assert( 6 === seq.length )
   }
 
   test( "symbol accessor" ) {
-    val dna = List( G, A, T, A, T , C )
-    val name = "Test sequence"
-    val seq = new Sequence( name, dna )
     assert( dna(2) === seq(2) )
   }
 
   test( "value equality" ) {
-    val dna = List( G, A, T, A, T , C )
-    val name = "Test sequence"
-    val seq = new Sequence( name, dna )
     val dna2 = List( G, A, T, A, T , C )
     val name2 = "Test sequence"
     val seq2 = new Sequence( name, dna2 )
     assert( seq == seq2 )
   }
+
+  test( "point mutation" ) {
+    var seq2 = seq.mutate( 2, G )
+    assert( seq != seq2 )
+    assert( seq.length === seq2.length )
+    assert( seq(2) == T )
+    assert( seq2(2) == G )
+  }
+
+  test( "String representation" ) {
+    assert( "gatatc" === seq.toString )
+  }
+
 }
