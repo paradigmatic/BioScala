9407460b768213cb85a1262cf9cc8447369639ee
JL Falcone <jean-luc.falcone@unige.ch>
2009-10-17 18:44:46 +0200

    Added methods to SymbolList

--- src/main/scala/symbols/SymbolList.scala
+++ src/main/scala/symbols/SymbolList.scala
@@ -1,8 +1,20 @@
 package bio.symbols
 
-class SymbolList[S <: Symbol] (private val lst: List[S]) {
+class SymbolList[S <: Symbol] (private val lst: List[S]) extends Iterable[S] {
 
   val length = lst.length
+  override val hasDefiniteSize = true
+  
+  // Biological index notation
+  def apply( i: Int ) = lst(i-1)
+
+  override def elements() = lst.elements
+  override def toList() = lst
+
+  def seqString() = lst.mkString("")
+
+  override def toString() = seqString
+
 
 }
 
--- src/test/scala/SymbolListTest.scala
+++ src/test/scala/SymbolListTest.scala
@@ -6,10 +6,23 @@ import org.scalatest._
 
 class SymbolListTest extends FunSuite with BeforeAndAfter {
   
+  val lst = List( A, T, G, C )
+  val symLst = new SymbolList( lst )
+
   test( "Empty List" ) {
   
     val empty = SymbolList()
     assert( 0 === empty.length )
+  }
+
+  test( "Element accessor" ) {
+    for( i <- 0 until lst.length ) {
+      assert( lst(i) === symLst( i + 1) )
+    }
+  }
+
+  test( "Sequence string" ) {
+    assert( "atgc" === symLst.seqString )
   }
   
 
