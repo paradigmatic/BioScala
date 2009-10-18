89f676bf4379e7c72ec14d3cc759f19b9591c56a
JL Falcone <jean-luc.falcone@unige.ch>
2009-10-18 21:47:10 +0200

    Adding license

--- src/main/scala/sym/Location.scala
+++ src/main/scala/sym/Location.scala
@@ -1,3 +1,24 @@
+/*  
+ *  This file is part of BioScala
+ *
+ *  BioScala is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  BioScala is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with BioScala.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  (c) 2009, Jean-Luc Falcone, jean-luc.falcone@unige.ch
+ *
+ */
+
+
 package bio.sym
 
 import Math._
