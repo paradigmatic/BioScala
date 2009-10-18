78a22049ac22e75e7603612838f359dd3cc30af6
JL Falcone <jean-luc.falcone@unige.ch>
2009-10-18 21:39:12 +0200

    Added location set operations

--- src/main/scala/sym/Location.scala
+++ src/main/scala/sym/Location.scala
@@ -1,13 +1,21 @@
 package bio.sym
 
+import Math._
+
+
 /**
  * Defines a location on a SymbolList or a Sequence.
  * The location bounds are inclusive and expressed in biological coordinates
  */
 
-class Location( val start: Int, val end: Int ) {
+case class Location( val start: Int, val end: Int ) {
   require( start > 0 )
   require( end  > 0 ) //TODO: check if requires are needed.
+
+  /**
+   * Returns the location length
+   */
+  def length = end - start + 1
 
   /**
    * Returns true if the location argument is totally included in this location
@@ -22,7 +30,6 @@ class Location( val start: Int, val end: Int ) {
   def overlaps( that: Location ) =
     ( this.end >= that.start && this.end <= that.end ) ||
     ( that.end >= this.start && that.end <= this.end )
-  
 
   /**
    * Return true if the argument location si contiguous but does not
@@ -31,9 +38,33 @@ class Location( val start: Int, val end: Int ) {
   def isContiguous( that: Location ) =
     this.end == that.start  - 1  ||  this.start == that.end + 1
   
+  /**
+   * Returns a new Location which is the intersection between
+   * this and the location argument. If the intersection is empty,
+   * throw an exception.
+   */
+  def intersection( that: Location ) = {
+    require( this.overlaps( that ) ) 
+    Location( max( this.start, that.start ), min( this.end, that.end ) )
+  }
 
-  override def toString() = "Location(" +start+"-"+end+")"
+  /**
+   * Return the union of this and the location argument. Throw an exception
+   * if there is neither overlap nor  continguity.
+   */
+  def union( that: Location ) = {
+    require( that.overlaps( this ) || that.isContiguous( this ) )
+    Location( min( this.start, that.start ), max( this.end, that.end ) )
+  }
+
+  override lazy val toString = "Location(" +start+"-"+end+")"
 
 }
 
-class PointLocation( val position: Int ) extends Location( position, position )
+case class PointLocation( val position: Int ) 
+  extends Location( position, position ) {
+    
+    override val length = 1
+
+    override lazy val toString = "PointLocation(" + position + ")"
+  }
--- src/test/scala/LocationTest.scala
+++ src/test/scala/LocationTest.scala
@@ -8,12 +8,12 @@ class LocationTest extends FunSuite with BeforeAndAfter {
 
   val min = 4
   val max = 10
-  val location = new Location( min, max )
-  val point = new PointLocation( (min+max)/2 )
+  val location = Location( min, max )
+  val point = PointLocation( (min+max)/2 )
 
   test( "Inclusion" ) {
     assert( location.includes( point ) )
-    val loc2 = new Location( min+1, max-1 )
+    val loc2 = Location( min+1, max-1 )
     assert( location.includes( loc2 ) )
     assert( location.includes( location ) )
   }
@@ -22,22 +22,40 @@ class LocationTest extends FunSuite with BeforeAndAfter {
     assert( location.overlaps( location ) )
     assert( location.overlaps( point ) )
     assert( point.overlaps( location ) )
-    val loc2 = new Location( min+2, max+4) 
+    val loc2 = Location( min+2, max+4) 
     assert( loc2.overlaps( location ) )
     assert( location.overlaps( loc2 ) )
-    val loc3 = new Location( max+1, max+3)
+    val loc3 = Location( max+1, max+3)
     assert( ! location.overlaps( loc3 ) )
   }
 
   test( "Contiguous" ) {
-    val loc2 = new Location( max+1, max +3 )
+    val loc2 = Location( max+1, max +3 )
     assert( loc2.isContiguous(location) )
-    val loc3 = new Location( min - 3, min - 1 )
+    val loc3 = Location( min - 3, min - 1 )
     assert( loc3.isContiguous(location) )
-    val loc4 = new Location( min, max+1 )
+    val loc4 = Location( min, max+1 )
     assert( ! loc4.isContiguous(location) )
-    val point2 = new PointLocation( max + 1 )
+    val point2 = PointLocation( max + 1 )
     assert( point2.isContiguous(location) )
+  }
+  
+  test( "Intersection" ) {
+    val location2 = Location( max-2, max+3 )
+    val intersection = location.intersection( location2 )
+    assert( intersection.start == max-2 )
+    assert( intersection.end == max )
+    assert( intersection == location2.intersection( location ) )
+    //TODO: test for exception
+  }
+
+  test( "Union" ) {
+    val location2 = Location( max-2, max+3 )
+    val union = location.union( location2 )
+    assert( union.start == min )
+    assert( union.end == max+3 )
+    //TODO: test for exception
+    assert( union == location2.union( location ) )
   }
 
 }
